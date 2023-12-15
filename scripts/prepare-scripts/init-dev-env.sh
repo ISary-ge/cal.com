a965822f68b514df11bc4543cfee0821f26b37d0
isary <igorek_sary@mail.ru>
2023-12-15 15:40:53 +0000

    step

--- scripts/prepare-scripts/init-dev-env.sh
+++ scripts/prepare-scripts/init-dev-env.sh
@@ -1,6 +1,7 @@
 set -e
 
 DIR=/workspace
+cd $DIR
 
 git clone git@github.com:ISary-ge/cal.com.git calendar --branch mcn-deploy || true
 cd calendar  && npx turbo prune --scope=@calcom/web && yarn workspace @calcom/web add
