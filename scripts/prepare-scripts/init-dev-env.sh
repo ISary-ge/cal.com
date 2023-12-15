2c5588544e1d51959ffb750a09810646cbc65504
isary <igorek_sary@mail.ru>
2023-12-15 16:56:42 +0300

    step

--- scripts/prepare-scripts/init-dev-env.sh
+++ scripts/prepare-scripts/init-dev-env.sh
@@ -3,4 +3,4 @@ set -e
 DIR=/workspace
 
 git clone git@github.com:ISary-ge/cal.com.git calendar --branch mcn-deploy || true
-cd calendar  && npx turbo prune --scope=@calcom/web
+cd calendar  && npx turbo prune --scope=@calcom/web && yarn workspace @calcom/web 
