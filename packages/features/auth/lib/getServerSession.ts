5e5f66bfdff3a39a65cb720f03041830975ac819
isary <igorek_sary@mail.ru>
2023-12-17 00:16:19 +0000

    step

--- packages/features/auth/lib/getServerSession.ts
+++ packages/features/auth/lib/getServerSession.ts
@@ -35,9 +35,7 @@ export async function getServerSession(options: {
   // });
 
   const mcnUidToken = req.cookies["mcn_uid"];
-  const mcnUserId = +(!isNaN(req.headers["mcn-user-id"])
-    ? (req.headers["mcn-user-id"] as string)
-    : ("0" as string));
+  const mcnUserId = +(req.headers["mcn-user-id"] as string);
 
   console.log("TOKEEEEn", req.cookies, mcnUserId);
 
