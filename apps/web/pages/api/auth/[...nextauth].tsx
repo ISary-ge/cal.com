5ded30a8b1f96056da2745a91901e358eabfe10f
isary <igorek_sary@mail.ru>
2023-12-17 11:07:44 +0000

    step

--- apps/web/pages/api/auth/[...nextauth].tsx
+++ apps/web/pages/api/auth/[...nextauth].tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
 import { defaultResponder } from "@calcom/lib/server";
 
 export default defaultResponder(async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log("))))))(()) config", process.env);
   const result = await getServerSession({ req, res });
   return result;
 });
